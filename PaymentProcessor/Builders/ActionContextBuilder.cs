ae176e37674f0aed0479949d489aa3a64822565d
Gene Graves <gemdevelopers@myokapis.net>
2025-05-25 19:57:07 -0500

    cleaned up builders and added an example context

---
+++ PaymentProcessor/Builders/ActionContextBuilder.cs
@@ -0,0 +1,22 @@
+﻿using PaymentProcessor.Transaction.Context;
+using PaymentProcessor.Transaction.Model;
+
+namespace PaymentProcessor.Builders
+{
+    public class ActionContextBuilder : IBuilder<ActionContext>
+    {
+        public ActionContext Build(ITransactionModel transaction)
+        {
+            var action = transaction.Details.Action.ToUpper();
+
+            return new ActionContext()
+            {
+                // TODO: map the remaining attributes
+                Auth = (action == "AUTH"),
+                Return = (action == "RETURN"),
+                Sale = (action == "SALE"),
+                TransactionType = action
+            };
+        }
+    }
+}
